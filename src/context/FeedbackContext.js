f3bcbc1d936d3a96e16bb5febb79f80de29e1408
Andres Roman <afromana@unal.edu.co>
2022-05-07 00:38:16 -0500

    Fake backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,26 @@
-import { createContext,useState } from "react";
-import {v4 as uuidv4} from 'uuid'
+import { createContext,useState,useEffect } from "react";
 
 const FeedbackContext=createContext()
 
 export const FeedbackProvider=({children})=>{
-    const [feedback,setFeedback]=useState([
-        {
-            id:1,
-            text:"This item is from context",
-            rating:10
-        },
-        {
-            id:2,
-            text:"This item is from context",
-            rating:9
-        },
-        {
-            id:3,
-            text:"This item is from context",
-            rating:7
-        }
-    ])
+    const [isLoading,setIsLoading]=useState(true)
+    const [feedback,setFeedback]=useState([])
 
     const [feedbackEdit,setFeedbackEdit]=useState({
         item:{},
         edit:false
     })
+
+    useEffect(()=>{
+        fetchFeedback()
+    },[])
+
+    const fetchFeedback= async ()=>{
+        const response=await fetch("/feedback")
+        const data=await response.json()
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     const editFeedback=(item)=>{
         setFeedbackEdit({
@@ -34,21 +29,43 @@ export const FeedbackProvider=({children})=>{
         })
     }
 
-    const deleteFeedback=(id)=>{
+    const deleteFeedback=async (id)=>{
         if(window.confirm('Are you sure you want to delete?')){
+            await fetch(`/feedback/${id}`,{
+                method:'DELETE'
+            })
             setFeedback(feedback.filter((item)=>item.id!==id))
         }
         
     }
 
-    const addFeedback=(newFeedback)=>{
-        newFeedback.id=uuidv4()
-        setFeedback([newFeedback,...feedback])
+    const addFeedback=async (newFeedback)=>{
+        const response=await fetch('/feedback',{
+            method:"POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(newFeedback)
+        })
+        
+        const data=await response.json()
+        setFeedback([data,...feedback])
     }
 
-    const updateFeedback=(id,uptItem)=>{
-        setFeedback(feedback.map((item)=>(item.id===id) ? 
-        {...item,...uptItem}:item))
+    const updateFeedback=async (id,uptItem)=>{
+        const response=await fetch(`/feedback/${id}`,{
+            method:'PUT',
+            header:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(uptItem)
+        })
+        const data=await response.json()
+        setFeedback(feedback.map((item)=>item.id===id ? data:item))
+        setFeedbackEdit({
+            item:{},
+            edit:false
+        })
     }
 
     return <FeedbackContext.Provider 
@@ -58,7 +75,8 @@ export const FeedbackProvider=({children})=>{
         addFeedback,
         editFeedback,
         feedbackEdit,
-        updateFeedback
+        updateFeedback,
+        isLoading
     }}>
         {children}
     </FeedbackContext.Provider>
